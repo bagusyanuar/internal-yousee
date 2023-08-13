ecb780296dd7d1d5444956cb07af4723e80c7996
bagusyanuar <bagus.yanuar613@gmail.com>
2023-08-13 15:16:34 +0700

    a

--- components/pages/advert-location/index.tsx
+++ components/pages/advert-location/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import MyCard from '../../card'
 import MyTable, { HeaderType } from '../../table'
+import Button from '../../form/button'
 
-const headers: Array<HeaderType> = [
-    { value: 'Email', className: 'w-1/5', childClassName: 'justify-center' },
-];
 
 function Index() {
     return (
@@ -12,7 +10,7 @@ function Index() {
             <div className='flex items-center mb-3'>
                 <div className='w-full flex items-center justify-between'>
 
-                    <span className='font-bold text-2xl text-slate-700'>Titik Iklan</span>
+                    <span className='font-semibold text-xl text-slate-700 tracking-wider'>Titik Iklan</span>
                     <nav className="flex" aria-label="Breadcrumb">
                         <ol className="inline-flex items-center space-x-1 md:space-x-3">
                             <li className="inline-flex items-center">
@@ -34,43 +32,79 @@ function Index() {
                 </div>
             </div>
             <MyCard className='py-4 px-4 w-100'>
+                <div className='flex justify-end'>
+                    <Button onClick={() => {}}>
+                        <span className='material-icons-round mr-2 text-sm'>add_circle_outline</span>
+                        <span className='tracking-wider text-sm'>Tambah Titik</span>
+                    </Button>
+                </div>
+                <hr className='mb-3 mt-3' />
+                <div className='flex items-center mb-3'>
+                    <span className='text-sm text-slate-600 me-2'>Show :</span>
+                    <select id='page_length' className='px-3 py-1 rounded-md border bg-inherit text-sm text-slate-600'>
+                        <option className='bg-white text-slate-600 hover:bg-slate-200' value={10}>10</option>
+                        <option className='bg-white text-slate-600 hover:bg-slate-200' value={25}>25</option>
+                        <option className='bg-white text-slate-600 hover:bg-slate-200' value={50}>50</option>
+                    </select>
+                    <span className='text-sm text-slate-600 ms-2'>entries</span>
+                </div>
                 <div className='relative overflow-auto shadow-md sm:rounded-lg border border-slate-200 mb-2'>
                     <table className='rounded-md overflow-x-scroll w-full text-sm text-left text-gray-500 dark:text-gray-400'>
                         <thead className="text-sm text-slate-600 bg-gray-50">
                             <tr>
-                                <td className={`px-6 py-3 w-1 text-center font-semibold`}>
+                                <td className='px-3 py-3 w-1 text-center font-semibold'>
                                     <span>#</span>
                                 </td>
-                                <td className={`px-6 py-3 w-1/5 text-left font-semibold`}>
+                                <td className='px-3 py-3 text-left font-semibold'>
                                     <span>Nama</span>
                                 </td>
-                                <td className={`px-6 py-3 w-1/5 text-left font-semibold`}>
+                                <td className='px-3 py-3 text-left font-semibold'>
                                     <span>Vendor</span>
                                 </td>
-                                <td className={`px-6 py-3 text-left font-semibold`}>
+                                <td className='px-3 py-3 text-left font-semibold'>
                                     <span>Alamat</span>
                                 </td>
-                                <td className={`px-6 py-3 w-2 text-left font-semibold`}>
+                                <td className='px-3 py-3 w-[5rem] text-center font-semibold'>
                                     <span>Posisi</span>
                                 </td>
-                                <td className={`px-6 py-3 w-2 text-left font-semibold`}>
-                                    <span>Dimensi</span>
+                                <td className='px-3 py-3 w-[6.5rem] text-center font-semibold'>
+                                    <span>Tinggi (M)</span>
                                 </td>
-                                <td className={`px-6 py-3 w-2 text-left font-semibold`}>
+                                <td className='px-3 py-3 w-[6.5rem] text-center font-semibold'>
+                                    <span>Lebar (M)</span>
+                                </td>
+                                <td className='px-3 py-3 w-[5rem] text-center font-semibold'>
                                     <span>Aksi</span>
                                 </td>
                             </tr>
                         </thead>
                         <tbody>
                             <tr className='bg-white border-b'>
-                                <td className={`px-6 py-3 text-gray-500 whitespace-nowrap w-1`}>
+                                <td className='px-3 py-3 text-center text-gray-500'>
                                     1
                                 </td>
-                                <td className={`px-6 py-3 text-gray-500 whitespace-nowrap w-1`}>
-                                    <span>Baliho Depan UNSA</span>
+                                <td className='px-3 py-3 text-gray-500'>
+                                    <span className='overflow-ellipsis line-clamp-3 text-justify'>Baliho Depan UNSA</span>
                                 </td>
-                                <td className={`px-6 py-3 text-gray-500 whitespace-nowrap w-1`}>
-                                    <span>MADE Digital Printing</span>
+                                <td className='px-3 py-3 text-gray-500'>
+                                    <span className='overflow-ellipsis line-clamp-3 text-justify'>MADE Digital Printing</span>
+                                </td>
+                                <td className='px-3 py-3 text-gray-500'>
+                                    <span className='overflow-ellipsis line-clamp-3 text-justify'>Jl. Kali Sindang No.RT.01 / 03, Jagalan, Kec. Jebres, Kota Surakarta, Jawa Tengah 57124</span>
+                                </td>
+                                <td className='px-3 py-3 text-center text-gray-500'>
+                                    <span className='overflow-ellipsis line-clamp-3 text-justify'>Vertical</span>
+                                </td>
+                                <td className='px-3 py-3 text-center text-gray-500'>
+                                    <span className='overflow-ellipsis line-clamp-3 text-center'>3.5</span>
+                                </td>
+                                <td className='px-3 py-3 text-center text-gray-500'>
+                                    <span className='overflow-ellipsis line-clamp-3 text-center'>3.5</span>
+                                </td>
+                                <td className='px-3 py-3 text-center text-gray-500'>
+                                    <span className="material-icons-round">
+                                        more_vert
+                                    </span>
                                 </td>
                             </tr>
                         </tbody>
